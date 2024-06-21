92fe15868bbada30c3fb1667a2475253a4d2d292
Jérôme MOUREY <jerome@everycheck.fr>
2024-06-21 12:02:43 +0200

    uncomment test and allow debug on test and clean useless printf

--- fcgi/fcgiclient/client_test.go
+++ fcgi/fcgiclient/client_test.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path"
 	"reflect"
+	"strings"
 	"testing"
 	"time"
 )
@@ -53,8 +54,8 @@ func TestDo(t *testing.T) {
 			t.Fatalf("failed to kill process: %v", err)
 		}
 	}()
-
-	// tooLongString := buildAStringOfLen(fcgiprotocol.MaxWrite)
+	//fcgiprotocol.DebugLog = os.Stdout
+	tooLongString := buildAStringOfLen(fcgiprotocol.MaxWrite)
 	toolongPairValue := buildAStringOfLen(fcgiprotocol.MaxPairLen)
 
 	tests := map[string]struct {
@@ -62,213 +63,213 @@ func TestDo(t *testing.T) {
 		Expected Response
 		Error    string
 	}{
-		// "main script not found": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "GET",
-		// 		Url:          MustUrl(t, "/"),
-		// 		Body:         "",
-		// 		Index:        "wrong.php",
-		// 		Env:          map[string]string{},
-		// 		Header:       map[string]string{},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 404,
-		// 		Header: map[string]string{
-		// 			"Content-type": "text/html; charset=UTF-8",
-		// 			"X-Powered-By": "PHP/8.3.7",
-		// 			"Status":       "404 Not Found",
-		// 		},
-		// 		Stdout: "File not found.\n",
-		// 		Stderr: "Primary script unknown",
-		// 	},
-		// },
-		// "basic": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "GET",
-		// 		Url:          MustUrl(t, "/"),
-		// 		Body:         "",
-		// 		Index:        "index.php",
-		// 		Env:          map[string]string{},
-		// 		Header:       map[string]string{},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 200,
-		// 		Header: map[string]string{
-		// 			"Content-type":  "text/html; charset=UTF-8",
-		// 			"X-Powered-By":  "PHP/8.3.7",
-		// 			"X-Request-Uri": "/",
-		// 		},
-		// 		Stdout: strings.Join([]string{
-		// 			"<h1>Requested URL:</h1>",
-		// 			"<p>/</p>",
-		// 			"<h1>Request Method:</h1>",
-		// 			"<p>GET</p>",
-		// 			"<h1>Headers:</h1>",
-		// 			"<pre>",
-		// 			"Content-Length: 0",
-		// 			"Content-Type: text/plain; charset=utf-8",
-		// 			"</pre>",
-		// 			"<h1>Body:</h1>",
-		// 			"<pre>",
-		// 			"</pre>",
-		// 		}, "\n"),
-		// 		Stderr: "",
-		// 	},
-		// },
-		// "basic with status code": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "GET",
-		// 		Url:          MustUrl(t, "/?status_code=403"),
-		// 		Body:         "",
-		// 		Index:        "index.php",
-		// 		Env:          map[string]string{},
-		// 		Header:       map[string]string{},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 403,
-		// 		Header: map[string]string{
-		// 			"Content-type":  "text/html; charset=UTF-8",
-		// 			"X-Powered-By":  "PHP/8.3.7",
-		// 			"X-Request-Uri": "/",
-		// 			"Status":        "403 Forbidden",
-		// 			"X-Status-Code": "403",
-		// 			"Status-Code":   "403",
-		// 		},
-		// 		Stdout: strings.Join([]string{
-		// 			"<h1>Requested URL:</h1>",
-		// 			"<p>/</p>",
-		// 			"<h1>Request Method:</h1>",
-		// 			"<p>GET</p>",
-		// 			"<h1>Headers:</h1>",
-		// 			"<pre>",
-		// 			"Content-Length: 0",
-		// 			"Content-Type: text/plain; charset=utf-8",
-		// 			"</pre>",
-		// 			"<h1>Body:</h1>",
-		// 			"<pre>",
-		// 			"</pre>",
-		// 		}, "\n"),
-		// 		Stderr: "",
-		// 	},
-		// },
-		// "option cors request": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "OPTIONS",
-		// 		Url:          MustUrl(t, "/api/users"),
-		// 		Body:         "",
-		// 		Index:        "index.php",
-		// 		Env:          map[string]string{},
-		// 		Header: map[string]string{
-		// 			"Access-Control-Request-Method":  "POST",
-		// 			"Access-Control-Request-Headers": "content-type",
-		// 			"Referer":                        "https://verification.exemple.com/",
-		// 			"Origin":                         "https://verification.exemple.com/",
-		// 		},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 200,
-		// 		Header: map[string]string{
-		// 			"Content-type":  "text/html; charset=UTF-8",
-		// 			"X-Powered-By":  "PHP/8.3.7",
-		// 			"X-Request-Uri": "/api/users",
-		// 		},
-		// 		Stdout: strings.Join([]string{
-		// 			"<h1>Requested URL:</h1>",
-		// 			"<p>/api/users</p>",
-		// 			"<h1>Request Method:</h1>",
-		// 			"<p>OPTIONS</p>",
-		// 			"<h1>Headers:</h1>",
-		// 			"<pre>",
-		// 			"Access-Control-Request-Headers: content-type",
-		// 			"Access-Control-Request-Method: POST",
-		// 			"Content-Length: 0",
-		// 			"Content-Type: text/plain; charset=utf-8",
-		// 			"Origin: https://verification.exemple.com/",
-		// 			"Referer: https://verification.exemple.com/",
-		// 			"</pre>",
-		// 			"<h1>Body:</h1>",
-		// 			"<pre>",
-		// 			"</pre>",
-		// 		}, "\n"),
-		// 		Stderr: "",
-		// 	},
-		// },
-		// "post json with body": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "POST",
-		// 		Url:          MustUrl(t, "/api/auth-tokens"),
-		// 		Body:         `{"login":admin","password":"azertyu"}` + "\n",
-		// 		Index:        "index.php",
-		// 		Env:          map[string]string{},
-		// 		Header: map[string]string{
-		// 			"Content-type": "application/json",
-		// 		},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 200,
-		// 		Header: map[string]string{
-		// 			"Content-type":  "text/html; charset=UTF-8",
-		// 			"X-Powered-By":  "PHP/8.3.7",
-		// 			"X-Request-Uri": "/api/auth-tokens",
-		// 		},
-		// 		Stdout: strings.Join([]string{
-		// 			"<h1>Requested URL:</h1>",
-		// 			"<p>/api/auth-tokens</p>",
-		// 			"<h1>Request Method:</h1>",
-		// 			"<p>POST</p>",
-		// 			"<h1>Headers:</h1>",
-		// 			"<pre>",
-		// 			"Content-Length: 38",
-		// 			"Content-Type: application/json",
-		// 			"</pre>",
-		// 			"<h1>Body:</h1>",
-		// 			"<pre>",
-		// 			`{"login":admin","password":"azertyu"}`,
-		// 			"</pre>",
-		// 		}, "\n"),
-		// 		Stderr: "",
-		// 	},
-		// },
-		// "body len over fcgiprotocol.MaxWrite": {
-		// 	In: Request{
-		// 		DocumentRoot: dir,
-		// 		Method:       "GET",
-		// 		Url:          MustUrl(t, "/"),
-		// 		Body:         "test: " + tooLongString + "\n",
-		// 		Index:        "index.php",
-		// 		Env:          map[string]string{},
-		// 		Header:       map[string]string{},
-		// 	},
-		// 	Expected: Response{
-		// 		StatusCode: 200,
-		// 		Header: map[string]string{
-		// 			"Content-type":  "text/html; charset=UTF-8",
-		// 			"X-Powered-By":  "PHP/8.3.7",
-		// 			"X-Request-Uri": "/",
-		// 		},
-		// 		Stdout: strings.Join([]string{
-		// 			"<h1>Requested URL:</h1>",
-		// 			"<p>/</p>",
-		// 			"<h1>Request Method:</h1>",
-		// 			"<p>GET</p>",
-		// 			"<h1>Headers:</h1>",
-		// 			"<pre>",
-		// 			"Content-Length: 65542",
-		// 			"Content-Type: text/plain; charset=utf-8",
-		// 			"</pre>",
-		// 			"<h1>Body:</h1>",
-		// 			"<pre>",
-		// 			"test: " + tooLongString,
-		// 			"</pre>",
-		// 		}, "\n"),
-		// 		Stderr: "",
-		// 	},
-		// },
+		"main script not found": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "GET",
+				Url:          MustUrl(t, "/"),
+				Body:         "",
+				Index:        "wrong.php",
+				Env:          map[string]string{},
+				Header:       map[string]string{},
+			},
+			Expected: Response{
+				StatusCode: 404,
+				Header: map[string]string{
+					"Content-type": "text/html; charset=UTF-8",
+					"X-Powered-By": "PHP/8.3.7",
+					"Status":       "404 Not Found",
+				},
+				Stdout: "File not found.\n",
+				Stderr: "Primary script unknown",
+			},
+		},
+		"basic": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "GET",
+				Url:          MustUrl(t, "/"),
+				Body:         "",
+				Index:        "index.php",
+				Env:          map[string]string{},
+				Header:       map[string]string{},
+			},
+			Expected: Response{
+				StatusCode: 200,
+				Header: map[string]string{
+					"Content-type":  "text/html; charset=UTF-8",
+					"X-Powered-By":  "PHP/8.3.7",
+					"X-Request-Uri": "/",
+				},
+				Stdout: strings.Join([]string{
+					"<h1>Requested URL:</h1>",
+					"<p>/</p>",
+					"<h1>Request Method:</h1>",
+					"<p>GET</p>",
+					"<h1>Headers:</h1>",
+					"<pre>",
+					"Content-Length: 0",
+					"Content-Type: text/plain; charset=utf-8",
+					"</pre>",
+					"<h1>Body:</h1>",
+					"<pre>",
+					"</pre>",
+				}, "\n"),
+				Stderr: "",
+			},
+		},
+		"basic with status code": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "GET",
+				Url:          MustUrl(t, "/?status_code=403"),
+				Body:         "",
+				Index:        "index.php",
+				Env:          map[string]string{},
+				Header:       map[string]string{},
+			},
+			Expected: Response{
+				StatusCode: 403,
+				Header: map[string]string{
+					"Content-type":  "text/html; charset=UTF-8",
+					"X-Powered-By":  "PHP/8.3.7",
+					"X-Request-Uri": "/",
+					"Status":        "403 Forbidden",
+					"X-Status-Code": "403",
+					"Status-Code":   "403",
+				},
+				Stdout: strings.Join([]string{
+					"<h1>Requested URL:</h1>",
+					"<p>/</p>",
+					"<h1>Request Method:</h1>",
+					"<p>GET</p>",
+					"<h1>Headers:</h1>",
+					"<pre>",
+					"Content-Length: 0",
+					"Content-Type: text/plain; charset=utf-8",
+					"</pre>",
+					"<h1>Body:</h1>",
+					"<pre>",
+					"</pre>",
+				}, "\n"),
+				Stderr: "",
+			},
+		},
+		"option cors request": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "OPTIONS",
+				Url:          MustUrl(t, "/api/users"),
+				Body:         "",
+				Index:        "index.php",
+				Env:          map[string]string{},
+				Header: map[string]string{
+					"Access-Control-Request-Method":  "POST",
+					"Access-Control-Request-Headers": "content-type",
+					"Referer":                        "https://verification.exemple.com/",
+					"Origin":                         "https://verification.exemple.com/",
+				},
+			},
+			Expected: Response{
+				StatusCode: 200,
+				Header: map[string]string{
+					"Content-type":  "text/html; charset=UTF-8",
+					"X-Powered-By":  "PHP/8.3.7",
+					"X-Request-Uri": "/api/users",
+				},
+				Stdout: strings.Join([]string{
+					"<h1>Requested URL:</h1>",
+					"<p>/api/users</p>",
+					"<h1>Request Method:</h1>",
+					"<p>OPTIONS</p>",
+					"<h1>Headers:</h1>",
+					"<pre>",
+					"Access-Control-Request-Headers: content-type",
+					"Access-Control-Request-Method: POST",
+					"Content-Length: 0",
+					"Content-Type: text/plain; charset=utf-8",
+					"Origin: https://verification.exemple.com/",
+					"Referer: https://verification.exemple.com/",
+					"</pre>",
+					"<h1>Body:</h1>",
+					"<pre>",
+					"</pre>",
+				}, "\n"),
+				Stderr: "",
+			},
+		},
+		"post json with body": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "POST",
+				Url:          MustUrl(t, "/api/auth-tokens"),
+				Body:         `{"login":admin","password":"azertyu"}` + "\n",
+				Index:        "index.php",
+				Env:          map[string]string{},
+				Header: map[string]string{
+					"Content-type": "application/json",
+				},
+			},
+			Expected: Response{
+				StatusCode: 200,
+				Header: map[string]string{
+					"Content-type":  "text/html; charset=UTF-8",
+					"X-Powered-By":  "PHP/8.3.7",
+					"X-Request-Uri": "/api/auth-tokens",
+				},
+				Stdout: strings.Join([]string{
+					"<h1>Requested URL:</h1>",
+					"<p>/api/auth-tokens</p>",
+					"<h1>Request Method:</h1>",
+					"<p>POST</p>",
+					"<h1>Headers:</h1>",
+					"<pre>",
+					"Content-Length: 38",
+					"Content-Type: application/json",
+					"</pre>",
+					"<h1>Body:</h1>",
+					"<pre>",
+					`{"login":admin","password":"azertyu"}`,
+					"</pre>",
+				}, "\n"),
+				Stderr: "",
+			},
+		},
+		"body len over fcgiprotocol.MaxWrite": {
+			In: Request{
+				DocumentRoot: dir,
+				Method:       "GET",
+				Url:          MustUrl(t, "/"),
+				Body:         "test: " + tooLongString + "\n",
+				Index:        "index.php",
+				Env:          map[string]string{},
+				Header:       map[string]string{},
+			},
+			Expected: Response{
+				StatusCode: 200,
+				Header: map[string]string{
+					"Content-type":  "text/html; charset=UTF-8",
+					"X-Powered-By":  "PHP/8.3.7",
+					"X-Request-Uri": "/",
+				},
+				Stdout: strings.Join([]string{
+					"<h1>Requested URL:</h1>",
+					"<p>/</p>",
+					"<h1>Request Method:</h1>",
+					"<p>GET</p>",
+					"<h1>Headers:</h1>",
+					"<pre>",
+					"Content-Length: 65542",
+					"Content-Type: text/plain; charset=utf-8",
+					"</pre>",
+					"<h1>Body:</h1>",
+					"<pre>",
+					"test: " + tooLongString,
+					"</pre>",
+				}, "\n"),
+				Stderr: "",
+			},
+		},
 		"too long pair fcgiprotocol.MaxKeyPairLen": {
 			In: Request{
 				DocumentRoot: dir,
@@ -282,7 +283,7 @@ func TestDo(t *testing.T) {
 				},
 			},
 			Expected: Response{},
-			Error:    "cannot send fcgi request: cant write req : build pair len of (65949) exceed MaMaxPairLen of (65535) : stderr ''",
+			Error:    "cannot send fcgi   request: cant write req : build pair len of (65949) exceed MaMaxPairLen of (65535) : stderr ''",
 		},
 	}
 
